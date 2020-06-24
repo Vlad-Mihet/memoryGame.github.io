17143aaaba3da2b449366ceebacb49eed7e641fe
Vlad Mihet <vlad.mihet@yahoo.com>
2020-06-24 11:29:15 +0300

    Improvements

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Initialize the first game
+
 var gameCards = document.getElementsByClassName('game-cell');
 var gameCells = document.getElementsByClassName('cell-content');
 var randomNumbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -9,6 +11,8 @@ var clicks = 0;
 var winCondition = false;
 var playAgainButton = document.getElementById('playAgain');
 
+// Make cards return to the state they were in at the beginning of the game; every new game
+
 function uncheckCards() {
     for (let i = 0; i < gameCards.length; i++) {
         gameCells[i].style.display = 'none';
@@ -16,6 +20,19 @@ function uncheckCards() {
         gameCards[i].classList.remove('guessed');
     }
 };
+
+// Clone all game cards, so the click event listener will be removed from each of them, and added again later on 
+
+function cloneGameCards() {
+    for (let i = 0; i < gameCards.length; i++) {
+        let xclone = gameCards[i].cloneNode(true);
+        xclone.appendChild[gameCells[i]];
+        xclone.classList.add('game-cell');
+        gameCards[i].parentNode.replaceChild(xclone, gameCards[i]);
+    }
+}
+
+// Add 'play-again' functionality to the play again button by resetting everything to the default every time a game has ended
 
 playAgainButton.addEventListener('click', () => {
     gameCards = document.getElementsByClassName('game-cell');
@@ -29,17 +46,20 @@ playAgainButton.addEventListener('click', () => {
     arrayLength = gameCells.length - 1;
     document.getElementById('table').style.display = 'block';
     document.getElementById('playAgainInterface').style.display = 'none';
+    cloneGameCards();
     uncheckCards();
     generateRandomCardsContent();
     showCardsOnClick();
 })
 
-// Create a play again function 
+// Create the win function in order to make the play-again interface appear after every finished game;
 
 function win() {
     document.getElementById('table').style.display = 'none';
     document.getElementById('playAgainInterface').style.display = 'block';
 };
+
+// Add onclick event listener in order to show the value of each card if it's being clicked.
 
 function showCardsOnClick() {
     for (let j = 0; j < gameCards.length; j++) {
@@ -58,28 +78,45 @@ function showCardsOnClick() {
     }
 };
 
+// A function used in order to generate the content of all cards randomly.
+
 function generateRandomCardsContent() {
     for (let i = 0; i < gameCells.length; i++) {
         let randomNumber = Math.floor(Math.random() * arrayLength);
         gameCells[i].innerText = randomNumbers[randomNumber];
-        console.log(randomNumbers[randomNumber]);
         const randomIndex = randomNumbers.indexOf(randomNumbers[randomNumber]);
         randomNumbers.splice(randomIndex, 1);
         arrayLength--;
     }
 };
 
+// A function used to check whether the 2 cards that can be selected at once do match or not.
+
 function checkIfMatch(array) {
     if (array[0] != array[1] && !gameCards[cardIndexes[0]].classList.contains('guessed') && !gameCards[cardIndexes[1]].classList.contains('guessed')) {
+        // If the cards don't match:
         gameCells[cardIndexes[0]].style.display = 'none';
         gameCells[cardIndexes[1]].style.display = 'none';
+
     } else if (gameCards[cardIndexes[0]].classList.contains('guessed') && (!gameCards[cardIndexes[1]].classList.contains('guessed'))) {
+        //  If any of the cards has already been guessed:
         gameCells[cardIndexes[1]].style.display = 'none';
+
     } else if (gameCards[cardIndexes[1]].classList.contains('guessed') && (!gameCards[cardIndexes[0]].classList.contains('guessed'))) {
+        //  If any of the cards has already been guessed:
         gameCells[cardIndexes[0]].style.display = 'none';
+
     } else if (gameCards[cardIndexes[1]].classList.contains('guessed') && (gameCards[cardIndexes[0]].classList.contains('guessed'))) {
+        // If both cards have been guessed:
         alert('Why would you even try that?');
+
+    } else if (cardIndexes[0] == cardIndexes[1]) {
+        // If the same card has been clicked twice
+        if (gameCards[cardIndexes[0]].classList.contains('guessed')) {
+            alert('You\'ve clicked it twice!');
+        }
     } else {
+        // If the cards match:
         gameCards[cardIndexes[0]].style.backgroundColor = 'green';
         gameCards[cardIndexes[1]].style.backgroundColor = 'green';
         gameCards[cardIndexes[0]].classList.add('guessed');
